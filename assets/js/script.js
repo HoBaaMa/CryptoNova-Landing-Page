45aa93b4465643082cc38cbf7be5d1f6d8668453
ehab <ehab.sadiq89@gmail.com>
2025-03-12 22:02:55 +0200

    Add ScrollReveal animations for hero section and sidebar; update hero text for clarity

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,16 @@ let links = document.querySelector('.links');
 menu.onclick = () => {
     menu.classList.toggle('bg-x');
     links.classList.toggle('open');
-}
+};
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.ScrollReveal('.hero-text', { delay: 200, origin: 'top' });
+sr.ScrollReveal('.hero-img', { delay: 450, origin: 'top' });
+sr.ScrollReveal('.sidebar', { delay: 500, origin: 'left' });
+sr.ScrollReveal('.scroll-down', { delay: 500, origin: 'right' });
