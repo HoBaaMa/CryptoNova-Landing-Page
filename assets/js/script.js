88976b50ab36cd8c339bafc60c9d16ca8395d068
ehab <ehab.sadiq89@gmail.com>
2025-03-12 22:10:15 +0200

    Refactor ScrollReveal initialization in script.js for improved readability and functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,13 @@ menu.onclick = () => {
     links.classList.toggle('open');
 };
 
-const sr = ScrollReveal({
+ScrollReveal({
     distance: '65px',
     duration: 2600,
     delay: 450,
     reset: true
-});
+}).reveal('.hero-text', { delay: 200, origin: 'top' });
 
-sr.ScrollReveal('.hero-text', { delay: 200, origin: 'top' });
-sr.ScrollReveal('.hero-img', { delay: 450, origin: 'top' });
-sr.ScrollReveal('.sidebar', { delay: 500, origin: 'left' });
-sr.ScrollReveal('.scroll-down', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.hero-img', { delay: 450, origin: 'top' });
+ScrollReveal().reveal('.sidebar', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.scroll-down', { delay: 500, origin: 'right' });
